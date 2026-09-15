a7b8c74dad8fab17b4f8ef61d1d87626b5e1cb91
ivodavhella <ivo.d.v.k@outlook.com>
2026-09-15 08:40:11 +0200

    most recent changes

--- TaskMaster_ToDoLy/Program.cs
+++ TaskMaster_ToDoLy/Program.cs
@@ -3,9 +3,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // testar git commands. (did not update on github after commit, add & push)
-        // it does not push the most recent
-        // it pushes the changes before tha last and most recent changes
+        // git add .
+         //   git commit -m "most recent changes"    
+        // git push -u origin main 
         //FileHandler.Open();
         ApplicationUI.Start();
     }
--- TaskMaster_ToDoLy/README.md
+++ TaskMaster_ToDoLy/README.md
@@ -13,9 +13,9 @@ Features:
 - XML documentation comments in all C# files
 
 Open `TaskMaster.csproj` in Visual Studio 2022 with the .NET 8 SDK and run with F5 or Ctrl+F5.
-git commit -m "testar git commands. did not update on github after commit, add & push"    
+
 
 git add .
-
+git commit -m "most recent changes"    
 git push -u origin main   
 git pull origin main --allow-unrelated-histories
